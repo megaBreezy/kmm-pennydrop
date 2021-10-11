367371b92e8ebc62c6e8322d324683886ef61a48
Brad Phillips <brad@bradphillips.org>
2021-10-11 17:18:09 -0500

    Connect game and pick player fragments to main activity within android app.

--- androidPennyDrop/src/main/java/com/megabreezy/pennydrop/android/MainActivity.kt
+++ androidPennyDrop/src/main/java/com/megabreezy/pennydrop/android/MainActivity.kt
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.megabreezy.pennydrop.Greeting
 import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.containerFragment) as NavHostFragment
+
+        this.navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(this.navController)
     }
 }
